a1309e571920f7eccf26282388f287d4909e2f09
abhishek7699 <abhishekyelluri@example.com>
2024-12-14 23:00:45 -0600

    new

---
+++ TIC_TAC_TOE/app.js
@@ -0,0 +1,10 @@
+let boxes=document.querySelectorAll(".box");
+
+let reset=document.querySelectorAll("#reset");
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+];
